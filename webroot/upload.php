7c0934659c5a12fe7b7147c8f28469e7d3d80829
Rafał Bernaczek <rafal@bernaczek.pl>
2014-07-30 16:31:17 +0200

    PHP parse gpx file

--- webroot/upload.php
+++ webroot/upload.php
@@ -1,21 +1,33 @@
 <?php
+if (!empty($_FILES)) {
+	$tempPath = $_FILES['file']['tmp_name'];
+	$uploadPath = dirname(__FILE__) . DIRECTORY_SEPARATOR . 'uploads' . DIRECTORY_SEPARATOR . $_FILES['file']['name'];
 
-if ( !empty( $_FILES ) ) {
+	move_uploaded_file($tempPath, $uploadPath);
+	// $uploadPath = "C:\workspace_php\poimod\webroot\uploads\Ciekawe_miejsca.gpx";
 
-    $tempPath = $_FILES[ 'file' ][ 'tmp_name' ];
-    $uploadPath = dirname( __FILE__ ) . DIRECTORY_SEPARATOR . 'uploads' . DIRECTORY_SEPARATOR . $_FILES[ 'file' ][ 'name' ];
+	$xml = simplexml_load_file($uploadPath);
+	$wpt = array ();
 
-    move_uploaded_file( $tempPath, $uploadPath );
+	foreach ($xml->wpt as $child) {
+		array_push($wpt, array (
+			'name' => $child->name . '',
+			'time' => $child->time . '',
+			'sym' => $child->sym . '',
+			'lat' => $child->attributes()->lat . '',
+			'lon' => $child->attributes()->lon . ''
+		));
+	}
 
-    $answer = array( 'answer' => 'File transfer completed' );
-    $json = json_encode( $answer );
-
-    echo $json;
-
-} else {
-
-    echo 'No files';
-
+	$answer = array (
+		'answer' => 'File transfer completed',
+		'wpt' => $wpt
+	);
+	$json = json_encode($answer);
+	echo $json;
+}
+else {
+	echo 'No files';
 }
 
 ?>
